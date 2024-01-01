9c6408defb410d08196f8837cdeffd1a7f7860c1
Tushar <rktushu@gmail.com>
2024-01-01 12:00:00 +0530

    Jan 1 commit

--- 4. Searching/binary_search.py
+++ 4. Searching/binary_search.py
@@ -1,24 +1,26 @@
 
+
+def bs(arr,x):
+
+    low = 0
+    high = len(arr) - 1
+    while low<=high:
+        mid = (low+high)//2
+        if arr[mid]==x:
+            # print("Found at -", mid + 1)
+            return mid
+        elif x<arr[mid]:
+            high = mid-1
+        elif x>arr[mid]:
+            low = mid+1
+    return "Not Found"
+
+
 arr=[5,12,23,40,45,50,55,60,65,70,75,80]
 
-x = 80
+x = 800
 
+print(bs(arr,x))
 
-low = 0
-high = len(arr) - 1
-
-
-while low<=high:
-    mid = (low+high)//2
-    if arr[mid]==x:
-        print("Found at -", mid + 1)
-        break
-    elif x<arr[mid]:
-        high = mid-1
-    elif x>arr[mid]:
-        low = mid+1
-    else:
-        print("Doesnt exist")
-
-    
+  
 
