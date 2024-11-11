a5ddf005cbc26dd88ccc7f6e00146af4ecc17ef5
Tushar <rktushu@gmail.com>
2024-11-11 17:49:29 +0530

    changes in gcd

--- mathematics/gcd.py
+++ mathematics/gcd.py
@@ -20,5 +20,33 @@ def gcd(a,b):
 #         return a
 #     return gcd(b, a % b)
 
-print(gcd(4,6))
+# print(gcd(4,6))
 
+
+
+def gccd(n1,n2):
+    # naive approach
+    # m = min(n1,n2)
+    # for i in range(m,0,-1):
+    #     if(n1%i == 0 and n2%i == 0):
+    #         # print(i)
+    #         hcf=i
+    
+    #         return hcf
+    
+    # euclidean approach (make both the numbers equal)
+    # while n1!=n2:
+    #     if n1>n2:
+    #         n1=n1-n2
+    #     else: n2 = n2-n1
+
+    # return n1      
+
+    # optimised euclidean approach (make b=0)
+    if n2==0:
+        return n1
+    else: 
+        # print(n2,n1)
+        return gccd(n2,n1%n2)    
+
+print(gccd(15,10))
