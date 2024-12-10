a2bf0745ae23c93e0f10edb61b4769becf8badf0
Tushar <rktushu@gmail.com>
2024-12-10 18:19:21 +0530

    random practice

---
+++ list/mean.py
@@ -0,0 +1,15 @@
+
+
+def mean(arr):
+    
+    # sum = 0
+    # for i in range(0,len(arr)):
+    #     sum+=arr[i]
+    
+    # return sum/len(arr)
+    return sum(arr)/len(arr)
+arr = [12,122,323,44,75]
+print(mean(arr))
+
+
+
