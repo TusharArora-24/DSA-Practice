f6ba6fae7f7e643eed6daf100ac88eab41cca097
Tushar <rktushu@gmail.com>
2024-12-13 15:29:13 +0530

    efficient second largest

--- list/second_largest.py
+++ list/second_largest.py
@@ -1,22 +1,47 @@
 
 def sec_largest(l):
-    
+    # using two loops O(n)
+    # if len(l)<=1:
+    #     return None
+    # large = l[0]
+    # large2 = None
+    # for i in l:
+    #     if large<i:
+    #         large = i
+    # print(large)
+    # for j in l:
+    #     # print(j)
+    #     if j!=large:
+    #         if large2 == None:
+    #             large2 = j
+    #         # print(j)
+    #         elif large2<j:
+    #             large2 = j
+
+    # return large2
+
+
+    #single traversal
     if len(l)<=1:
         return None
-    large = l[0]
-    large2 = 0
-    for i in l:
-        if large<i:
-            large = i
-    print(large)
-    for j in l:
-        # print(j)
-        if j<large and large2<j:
-            # print(j)
-            large2 = j
-    return large2
+    
+    lar = l[0]
+    slar = None
+    # print(lar,slar)
+    for x in l[1:]:
+        
+        if x>lar:
+            slar = lar
+            lar = x
+            # print(slar,lar)
+        elif x<lar:
+            if slar == None or slar<x:
+                slar=x
+    return slar
 
-l = [20,10,20,8,12]
+
+
+l = [2,1,1]
 print(sec_largest(l))
 
 
