b6fbba397f390b83badcee861c4fc3817bfdf28d
Tushar <rktushu@gmail.com>
2024-12-16 15:17:34 +0530

    left rotate

--- list/left_rotate.py
+++ list/left_rotate.py
@@ -1,12 +1,21 @@
 
 
-def left_rotate(l):
+def left_rotate(l,n):
 
-    l[0] = l[1]
-    l[1] = l[0]
+    
+    if len(l)<=1:
+        return "Cant be rotated"
+    temp = l[0]
+
+    for i in range(1,n):
+        l[i-1] = l[i]
+    
+    l[n-1] = temp
+    # l.append(l.pop(0))
     return l
+    
 
-print(left_rotate(l=[1,2,3,4,5]))
+print(left_rotate(l=[1,2,3,4,5],n=5))
 
 
 
