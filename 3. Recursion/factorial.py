ff08f22d3db5e302a2d23051d1d13c2d58785b8f
Tushar <rktushu@gmail.com>
2024-12-19 15:34:22 +0530

    factorial with and without tail recursion

---
+++ 3. Recursion/factorial.py
@@ -0,0 +1,15 @@
+
+
+# def fact(n):
+
+#     if n==1:
+#         return 1
+#     return n*fact(n-1)
+
+def factTR(n,a):
+    if n==0:
+        return a
+    return (factTR(n-1,n*a))
+
+print(factTR(5,1))
+
