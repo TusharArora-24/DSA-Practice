5f137bc6e7cfb10c1f790456274a6f6dccc699b2
Tushar <rktushu@gmail.com>
2025-01-08 11:09:34 +0530

    corection in binary search

--- 4. Searching/binary_search.py
+++ 4. Searching/binary_search.py
@@ -1,7 +1,7 @@
 
-arr=[5,12,23,40,45,50]
+arr=[5,12,23,40,45,50,55,60,65,70,75,80]
 
-x = 12
+x = 80
 
 
 low = 0
@@ -12,6 +12,7 @@ while low<=high:
     mid = (low+high)//2
     if arr[mid]==x:
         print("Found at -", mid + 1)
+        break
     elif x<arr[mid]:
         high = mid-1
     elif x>arr[mid]:
