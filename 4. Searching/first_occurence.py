1f6ddc0a38fc5116dc272d43d842d477e4d27376
Tushar <rktushu@gmail.com>
2025-01-14 16:51:56 +0530

    first occurence efficient

--- 4. Searching/first_occurence.py
+++ 4. Searching/first_occurence.py
@@ -2,20 +2,32 @@
 
 def fo(arr,k,low,high):
     
-    if low>high:
-        return -1
+    # if low>high:
+    #     return -1
     
-    mid = (low+high)//2
-    # print(arr[mid])
-    if arr[mid]>k:
-        return fo(arr,k,low,mid-1)
-    elif arr[mid]<k:
-        return fo(arr,k,mid+1,high)
-    else:
-        if mid == 0 or arr[mid-1]!=arr[mid]:
-            return mid + 1
-        else: return fo(arr,k,low,mid-1)
+    # mid = (low+high)//2
+    # # print(arr[mid])
+    # if arr[mid]>k:
+    #     return fo(arr,k,low,mid-1)
+    # elif arr[mid]<k:
+    #     return fo(arr,k,mid+1,high)
+    # else:
+    #     if mid == 0 or arr[mid-1]!=arr[mid]:
+    #         return mid + 1
+    #     else: return fo(arr,k,low,mid-1)
 
+    while low<=high:
+        mid = (low+high)//2
+        # print(mid)
+        if arr[mid]>k:
+            high = mid-1
+        elif arr[mid]<k:
+            low = mid+1
+        else: 
+            if arr[mid-1]!=arr[mid] or mid == 0:
+                return mid +1
+            else:high = mid - 1
+    return False
 
 arr = [10,10,20,20,30,40,40,40]
 x = 20
