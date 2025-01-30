ede02bb4d53772e58ab7a0fa1050e57ddda9648d
Tushar <rktushu@gmail.com>
2025-01-30 17:51:05 +0530

    count 1's in increasing order array

---
+++ 4. Searching/count_1's.py
@@ -0,0 +1,27 @@
+
+
+def count(arr):
+    
+    n = len(arr)
+    start = 0
+    end = n-1
+
+    while start <= end:
+        
+        mid = (start+end)//2
+        
+        if arr[mid]==0:
+            start = mid + 1
+        else:
+            if mid==0 or arr[mid-1]!=1:
+                # print(end,mid)
+                return ((len(arr)-1)-mid+1)
+            else:
+                end = mid - 1
+    return 0
+
+
+arr = [1, 1, 1, 1, 0, 0, 0]
+print(count(arr))
+
+
