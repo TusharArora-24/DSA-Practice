673be53c129d1d87a33dd022a842368d9b87b552
Tushar <rktushu@gmail.com>
2025-01-30 17:58:47 +0530

    count 1's in non-increasing order array

--- 4. Searching/count_1's.py
+++ 4. Searching/count_1's.py
@@ -11,17 +11,22 @@ def count(arr):
         mid = (start+end)//2
         
         if arr[mid]==0:
-            start = mid + 1
+            # start = mid + 1
+            end = mid - 1
         else:
-            if mid==0 or arr[mid-1]!=1:
-                # print(end,mid)
-                return ((len(arr)-1)-mid+1)
-            else:
-                end = mid - 1
+            # if mid==0 or arr[mid-1]!=1:
+            #     # print(end,mid)
+            #     return ((len(arr)-1)-mid+1)
+            # else:
+            #     end = mid - 1
+            if mid == len(arr)-1 or arr[mid+1]!=1:
+                return mid + 1
+            else: 
+                start = mid + 1
     return 0
 
 
-arr = [1, 1, 1, 1, 0, 0, 0]
+arr = [1,0,0,0]
 print(count(arr))
 
 
