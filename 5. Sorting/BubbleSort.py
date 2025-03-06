18d2cbda7d3f504f8f638dec1b5030411d9c95c0
Tushar <rktushu@gmail.com>
2025-03-06 15:24:51 +0530

    bbuble sort (op)

--- 5. Sorting/BubbleSort.py
+++ 5. Sorting/BubbleSort.py
@@ -5,14 +5,18 @@ def Bsort(arr):
     n = len(arr)
 
     for i in range(n-1):
+        swapped = False
         for j in range(n-i-1):
             if arr[j]>arr[j+1]:
                 arr[j],arr[j+1] = arr[j+1],arr[j]
-        
+                swapped = True
+        if swapped == False:
+            print("Here")
+            return arr
     return arr
 
 
-arr = [10,4,2,5]
+arr = [20, 40, 50, 10]
 
 print(Bsort(arr))
 
