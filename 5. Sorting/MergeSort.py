57856d9d35ae7e2b21402a30b830650d109fa9db
Tushar <rktushu@gmail.com>
2025-03-26 17:44:14 +0530

    merge sort

--- 5. Sorting/MergeSort.py
+++ 5. Sorting/MergeSort.py
@@ -3,20 +3,31 @@
 def merge(arr1,arr2):
 
     final = []
+    m = len(arr1)
+    n = len(arr2)
+    i = 0
+    j = 0
 
-    for i in range(0,len(arr1)):
-        for j in range(0,len(arr2)):
-            if arr1[i]<arr2[j]:
-                final.append(arr1[i])
-                break
-            elif arr2[j]<arr1[i]:
-                final.append(arr2[j])
-                j=j+1
+    while i<m and j<n:
+        if arr1[i]<arr2[j]:
+            final.append(arr1[i])
+            i=i+1
+        else:
+            final.append(arr2[j])
+            j=j+1
+    
+    while i<m:
+        final.append(arr1[i])
+        i=i+1
+    while j<n:
+        final.append(arr2[j])
+        j=j+1
+
 
     print(final)
 
-arr1 = [20,25,30]
-arr2 = [11,27]
+arr1 = [10, 15]
+arr2 = [5, 6, 6, 30, 40]
 
 merge(arr1,arr2)
 
