ec6ec8b3f03c8873d2ab39e822c282dd93a4dc67
TusharH <rktushu@gmail.com>
2025-04-22 17:41:38 +0530

    mergesort final

--- 5. Sorting/MergeSubArrays.py
+++ 5. Sorting/MergeSubArrays.py
@@ -2,7 +2,7 @@
 
 def msa(arr,low,mid,high):
     
-    left = arr[0:mid + 1]
+    left = arr[low:mid + 1]
     right = arr[mid+1:high+1]
     i = 0
     j = 0
@@ -24,44 +24,27 @@ def msa(arr,low,mid,high):
         arr[k] = left[i]
         i = i+1
         k=k+1
-    print(j)
+    # print(j)
     while j<len(right):
         # if arr[i]<arr[j]:
         arr[k] = right[j]
         j = j+1
         k=k+1
-    # while i <= mid:
-    #     left.append(arr[i])
-    #     i=i+1
-    # while j<=high:
-    #     right.append(arr[j])
-    #     j=j+1
-    # m = 0
-    # n = 0
 
-    # while m<len(left) and n<len(right):
-    #     if left[m]<right[n]:
-    #         final.append(left[m])
-    #         m=m+1
-    #     else: 
-    #         final.append(right[n])
-    #         n=n+1
-    
-    # while m<len(left):
-        
-    #     final.append(left[m])
-    #     m=m+1
-        
-    # while n<len(right):
+    # print(arr)
 
-    #     final.append(right[n])
-    #     n=n+1
+def MS(arr,l,r):
 
+    if l<r:
+        m=(l+r)//2
+        MS(arr,l,m)
+        MS(arr,m+1,r)
+        msa(arr,l,m,r)
 
-    # pass
-    print(arr)
-arr = [10,15,20,11,13]
-msa(arr,0,2,4)
+        print(arr)
+
+arr = [10,5,30,15,7]
+MS(arr,0,4)
 
 
 
